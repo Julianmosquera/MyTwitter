d05d284ef3bf21d0f3808ca93c4cb8a528735da8
julianMGCODE <julianmosquerag@gmail.com>
2016-04-11 09:01:23 +0200

    Inicial

---
+++ src/twitter4j/Twitter4j.java
@@ -0,0 +1,16 @@
+
+package twitter4j;
+
+/**
+ *
+ * @author julian
+ */
+public class Twitter4j {
+
+   
+    public static void main(String[] args) {
+        Twitter twitter = new TwitterFactory().getInstance();
+        
+    }
+    
+}
