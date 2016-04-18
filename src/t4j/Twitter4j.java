66cfb6c56f58e0da4198bc10c870757c9c391eb9
julianMGCODE <julianmosquerag@gmail.com>
2016-04-18 09:30:22 +0200

    fix

---
+++ src/t4j/Twitter4j.java
@@ -0,0 +1,19 @@
+
+package t4j;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+/**
+ *
+ * @author julian
+ */
+public class Twitter4j {
+
+   
+    public static void main(String[] args) {
+        Twitter twitter = new TwitterFactory().getInstance();
+        
+    }
+    
+}
