584d0279d35c56a29aa40459b13f6475cdd6ab69
mmoureperez <mmoureperez@atanasoff02.danielcastelao.org>
2016-05-05 09:28:23 +0200

    modificaciones Mario

--- src/t4j/T4j.java
+++ src/t4j/T4j.java
@@ -2,14 +2,8 @@
 package t4j;
 
 import java.util.List;
-import twitter4j.DirectMessage;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.*;
+import static twitter4j.Query.Unit.mi;
 
 /**
  *
@@ -17,7 +11,6 @@ import twitter4j.conf.ConfigurationBuilder;
  */
 public class T4j {
 
-   
     public static void main(String[] args) throws TwitterException {
         //Creamos el objeto builder de confi
 
@@ -27,55 +20,37 @@ public class T4j {
 //                .setOAuthConsumerSecret("pJjfCDd5b1hsMhVPEmwt4LgAYOBcctjrjdOIofhWI1YNk065f5")
 //                .setOAuthAccessToken("184950863-WLuPkOH8BwWRV8c25ASOsXnOMfu340fDhOwymcAI")
 //                .setOAuthAccessTokenSecret("03VqDdFGjCkl4od3NGT2MZ0SQYyWwE1SxhskIM2s6z3Mv");
-     // TwitterFactory tf = new TwitterFactory(cb.build());
-  
+        // TwitterFactory tf = new TwitterFactory(cb.build());
         //sin configuration builder
-       Twitter mitwitter = new TwitterFactory().getInstance();
-        
-  
+        Twitter mitwitter = new TwitterFactory().getInstance();
 
    //post
-        
 //        Status mistatus = mitwitter.updateStatus("Probando en Java");
 //    System.out.println("Successfully updated the status to [" + mistatus.getText() + "].");
 //        
-    //creando via propeties
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        //creando via propeties
+        Status statusMario = mitwitter.updateStatus("Julian deja de copiarme tanto");
+
     //timeline
-    
 //    Twitter twitter = TwitterFactory.getSingleton();
-    List<Status> statuses = mitwitter.getHomeTimeline();
-    System.out.println("Showing home timeline.");
-    for (Status status : statuses) {
-        System.out.println(status.getUser().getName() + ":" +
-                           status.getText());
-    }
+        List<Status> statuses = mitwitter.getHomeTimeline();
+        System.out.println("Showing home timeline.");
+        for (Status status : statuses) {
+            System.out.println(status.getUser().getName() + ":"
+                    + status.getText());
+        }
 
-     Query query = new Query("hola");
-    QueryResult result = mitwitter.search(query);
-    for (Status status : result.getTweets()) {
-        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-    }
+        Query query = new Query("hola");
+        QueryResult result = mitwitter.search(query);
+        for (Status status : result.getTweets()) {
+            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+        }
 
-    
-    
     //DM
-    
 //    
 //    DirectMessage message = mitwitter.sendDirectMessage(, message);
 //    System.out.println("Sent: " message.getText() + " to @" + message.getRecipientScreenName());
 //        
 //    }
-
-}
+    }
 }
